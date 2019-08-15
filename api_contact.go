f662530c1be08c4dc1822fcfa4b44f5a1c2dd359
lixinxiang <lixinxiang@dingtalk.com>
2019-08-15 18:48:18 +0800

    user/list 更换成 user/listbypage

--- api_contact.go
+++ api_contact.go
@@ -7,23 +7,41 @@ import (
 
 type User struct {
 	OAPIResponse
-	Userid     string
-	Name       string
-	Mobile     string
-	Tel        string
-	Remark     string
-	Order      int
-	IsAdmin    bool
-	IsBoss     bool
-	IsLeader   bool
-	IsSys      bool `json:"is_sys"`
-	SysLevel   int  `json:"sys_level"`
-	Active     bool
-	Department []int
-	Position   string
-	Email      string
-	Avatar     string
-	Extattr    interface{}
+	Userid          string
+	IsSys           bool `json:"is_sys"`
+	SysLevel        int  `json:"sys_level"`
+	Order           int
+	IsLeader        bool
+	Unionid         string      `json:"unionid"`         // 员工在当前开发者企业账号范围内的唯一标识，系统生成，固定值，不会改变
+	Name            string      `json:"name"`            // 员工名字
+	TEL             string      `json:"tel"`             // 分机号（仅限企业内部开发调用）
+	WorkPlace       string      `json:"workPlace"`       // workPlace 办公地点
+	Remark          string      `json:"remark"`          // remark 备注
+	Mobile          string      `json:"mobile"`          // mobile 手机号码
+	Email           string      `json:"email"`           // email 员工的电子邮箱
+	OrgEmail        string      `json:"orgEmail"`        // orgEmail 员工的企业邮箱，如果员工已经开通了企业邮箱，接口会返回，否则不会返回
+	Active          bool        `json:"active"`          // active 是否已经激活，true表示已激活，false表示未激活
+	OrderInDepts    string      `json:"orderInDepts"`    // orderInDepts 在对应的部门中的排序，Map结构的json字符串，key是部门的Id，value是人员在这个部门的排序值
+	IsAdmin         bool        `json:"isAdmin"`         // isAdmin 是否为企业的管理员，true表示是，false表示不是
+	IsBoss          bool        `json:"isBoss"`          // isBoss 是否为企业的老板，true表示是，false表示不是
+	IsLeaderInDepts string      `json:"isLeaderInDepts"` // isLeaderInDepts 在对应的部门中是否为主管：Map结构的json字符串，key是部门的Id，value是人员在这个部门中是否为主管，true表示是，false表示不是
+	IsHide          string      `json:"isHide"`          // isHide 是否号码隐藏，true表示隐藏，false表示不隐藏
+	Department      string      `json:"department"`      // department 成员所属部门id列表
+	Position        string      `json:"position"`        // position 职位信息
+	Avatar          string      `json:"avatar"`          // avatar 头像url
+	HiredDate       string      `json:"hiredDate"`       // hiredDate 入职时间。Unix时间戳 （在OA后台通讯录中的员工基础信息中维护过入职时间才会返回)
+	Jobnumber       string      `json:"jobnumber"`       // jobnumber 员工工号
+	Extattr         interface{} `json:"extattr"`         // extattr 扩展属性，可以设置多种属性（但手机上最多只能显示10个扩展属性，具体显示哪些属性，请到OA管理后台->设置->通讯录信息设置和OA管理后台->设置->手机端显示信息设置）
+	IsSenior        bool        `json:"isSenior"`        // isSenior 是否是高管
+	StateCode       string      `json:"stateCode"`       // stateCode 国家地区码
+	Roles           []Role      `json:"roles"`           // roles 用户所在角色列表
+}
+
+//Role 角色
+type Role struct {
+	ID        string `json:"id"`        //角色id
+	Name      string `json:"name"`      //角色名称
+	GroupName string `json:"groupName"` //角色组名称
 }
 
 type UserList struct {
@@ -80,7 +98,7 @@ func (c *DingTalkClient) UserList(departmentID, offset, size int) (UserList, err
 	params.Add("department_id", fmt.Sprintf("%d", departmentID))
 	params.Add("offset", fmt.Sprintf("%d", offset))
 	params.Add("size", fmt.Sprintf("%d", size))
-	err := c.httpRPC("user/list", params, nil, &data)
+	err := c.httpRPC("user/listbypage", params, nil, &data)
 	return data, err
 }
 
